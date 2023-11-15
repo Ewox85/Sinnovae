1b0bb23f4c57478c9da5036b5373affb3475a76b
etud user <etud@port-hp-probook650G8-1>
2023-11-15 10:08:12 +0100

    new masuda

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+var tabSandwich = document.getElementById("sandwich");
+
+function myFunction() {
+    var tabSandwich = document.getElementById("sandwich");
+    var fleche = document.getElementById("fleche");
+    if (tabSandwich.style.display === "none") {
+        tabSandwich.style.display = "";
+        fleche.innerHTML = "▼";
+    } else {
+        tabSandwich.style.display = "none";
+        fleche.innerHTML = "▲";
+    }
+}
