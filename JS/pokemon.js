7416c767e230405faff23ed8df1be033fcade0c3
etud user <etud@debian>
2023-12-06 10:55:47 +0000

    add outils random shasse

---
+++ JS/pokemon.js
@@ -0,0 +1,14 @@
+const pokemonList = [
+    {
+		"name": "salameche",
+        "numero": "004",
+	},
+    {
+		"name": "reptincel",
+        "numero": "005",
+	},
+    {
+		"name": "dracaufeu",
+        "numero": "006",
+	},
+]
