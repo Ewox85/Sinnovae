1a5dff515ef545606bb6c703378ba47c29054734
etud user <etud@debian>
2023-12-06 11:06:21 +0000

    add numero poke

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -1,14 +1,598 @@
 const pokemonList = [
     {
-		"name": "bulbizarre",
+        "name": "bulbizarre",
         "numero": "001",
-	},
+    },
     {
-		"name": "herbizarre",
+        "name": "herbizarre",
         "numero": "002",
-	},
+    },
     {
-		"name": "florizarre",
+        "name": "florizarre",
         "numero": "003",
-	},
-]
+    },
+    {
+        "name": "salameche",
+        "numero": "004",
+    },
+    {
+        "name": "reptincel",
+        "numero": "005",
+    },
+    {
+        "name": "dracaufeu",
+        "numero": "006",
+    },
+    {
+        "name": "carapuce",
+        "numero": "007",
+    },
+    {
+        "name": "carabaffe",
+        "numero": "008",
+    },
+    {
+        "name": "tortank",
+        "numero": "009",
+    },
+    {
+        "name": "chenipan",
+        "numero": "010",
+    },
+    {
+        "name": "chrysacier",
+        "numero": "011",
+    },
+    {
+        "name": "papilusion",
+        "numero": "012",
+    },
+    {
+        "name": "aspicot",
+        "numero": "013",
+    },
+    {
+        "name": "coconfort",
+        "numero": "014",
+    },
+    {
+        "name": "dardargnan",
+        "numero": "015",
+    },
+    {
+        "name": "roucool",
+        "numero": "016",
+    },
+    {
+        "name": "roucoups",
+        "numero": "017",
+    },
+    {
+        "name": "roucarnage",
+        "numero": "018",
+    },
+    {
+        "name": "rattata",
+        "numero": "019",
+    },
+    {
+        "name": "rattatac",
+        "numero": "020",
+    },
+    {
+        "name": "piafabec",
+        "numero": "021",
+    },
+    {
+        "name": "rapasdepic",
+        "numero": "022",
+    },
+    {
+        "name": "abo",
+        "numero": "023",
+    },
+    {
+        "name": "arbok",
+        "numero": "024",
+    },
+    {
+        "name": "pikachu",
+        "numero": "025",
+    },
+    {
+        "name": "raichu",
+        "numero": "026",
+    },
+    {
+        "name": "sabelette",
+        "numero": "027",
+    },
+    {
+        "name": "sablaireau",
+        "numero": "028",
+    },
+    {
+        "name": "nidoran♀",
+        "numero": "029",
+    },
+    {
+        "name": "nidorina",
+        "numero": "030",
+    },
+    {
+        "name": "nidoqueen",
+        "numero": "031",
+    },
+    {
+        "name": "nidoran♂",
+        "numero": "032",
+    },
+    {
+        "name": "nidorino",
+        "numero": "033",
+    },
+    {
+        "name": "nidoking",
+        "numero": "034",
+    },
+    {
+        "name": "melofee",
+        "numero": "035",
+    },
+    {
+        "name": "melodelfe",
+        "numero": "036",
+    },
+    {
+        "name": "goupix",
+        "numero": "037",
+    },
+    {
+        "name": "feunard",
+        "numero": "038",
+    },
+    {
+        "name": "rondoudou",
+        "numero": "039",
+    },
+    {
+        "name": "grodoudou",
+        "numero": "040",
+    },
+    {
+        "name": "nosferapti",
+        "numero": "041",
+    },
+    {
+        "name": "nosferalto",
+        "numero": "042",
+    },
+    {
+        "name": "mystherbe",
+        "numero": "043",
+    },
+    {
+        "name": "ortide",
+        "numero": "044",
+    },
+    {
+        "name": "rafflesia",
+        "numero": "045",
+    },
+    {
+        "name": "paras",
+        "numero": "046",
+    },
+    {
+        "name": "parasect",
+        "numero": "047",
+    },
+    {
+        "name": "mimitoss",
+        "numero": "048",
+    },
+    {
+        "name": "aeromite",
+        "numero": "049",
+    },
+    {
+        "name": "taupiqueur",
+        "numero": "050",
+    },
+    {
+        "name": "triopikeur",
+        "numero": "051",
+    },
+    {
+        "name": "miaouss",
+        "numero": "052",
+    },
+    {
+        "name": "persian",
+        "numero": "053",
+    },
+    {
+        "name": "psykokwak",
+        "numero": "054",
+    },
+    {
+        "name": "akwakwak",
+        "numero": "055",
+    },
+    {
+        "name": "férosinge",
+        "numero": "056",
+    },
+    {
+        "name": "colossinge",
+        "numero": "057",
+    },
+    {
+        "name": "caninos",
+        "numero": "058",
+    },
+    {
+        "name": "arcanin",
+        "numero": "059",
+    },
+    {
+        "name": "ptitard",
+        "numero": "060",
+    },
+    {
+        "name": "têtarte",
+        "numero": "061",
+    },
+    {
+        "name": "tartard",
+        "numero": "062",
+    },
+    {
+        "name": "abra",
+        "numero": "063",
+    },
+    {
+        "name": "kadabra",
+        "numero": "064",
+    },
+    {
+        "name": "alakazam",
+        "numero": "065",
+    },
+    {
+        "name": "machoc",
+        "numero": "066",
+    },
+    {
+        "name": "machopeur",
+        "numero": "067",
+    },
+    {
+        "name": "mackogneur",
+        "numero": "068",
+    },
+    {
+        "name": "chetiflor",
+        "numero": "069",
+    },
+    {
+        "name": "boustiflor",
+        "numero": "070",
+    },
+    {
+        "name": "empiflor",
+        "numero": "071",
+    },
+    {
+        "name": "tentacool",
+        "numero": "072",
+    },
+    {
+        "name": "tentacruel",
+        "numero": "073",
+    },
+    {
+        "name": "racaillou",
+        "numero": "074",
+    },
+    {
+        "name": "gravalanch",
+        "numero": "075",
+    },
+    {
+        "name": "grolem",
+        "numero": "076",
+    },
+    {
+        "name": "ponyta",
+        "numero": "077",
+    },
+    {
+        "name": "galopa",
+        "numero": "078",
+    },
+    {
+        "name": "ramoloss",
+        "numero": "079",
+    },
+    {
+        "name": "flagadoss",
+        "numero": "080",
+    },
+    {
+        "name": "magneti",
+        "numero": "081",
+    },
+    {
+        "name": "magneton",
+        "numero": "082",
+    },
+    {
+        "name": "canarticho",
+        "numero": "083",
+    },
+    {
+        "name": "doduo",
+        "numero": "084",
+    },
+    {
+        "name": "dodrio",
+        "numero": "085",
+    },
+    {
+        "name": "otaria",
+        "numero": "086",
+    },
+    {
+        "name": "lamantine",
+        "numero": "087",
+    },
+    {
+        "name": "tadmorv",
+        "numero": "088",
+    },
+    {
+        "name": "grotadmorv",
+        "numero": "089",
+    },
+    {
+        "name": "kokiyas",
+        "numero": "090",
+    },
+    {
+        "name": "crustabri",
+        "numero": "091",
+    },
+    {
+        "name": "fantominus",
+        "numero": "092",
+    },
+    {
+        "name": "spectrum",
+        "numero": "093",
+    },
+    {
+        "name": "ectoplasma",
+        "numero": "094",
+    },
+    {
+        "name": "onix",
+        "numero": "095",
+    },
+    {
+        "name": "soporifik",
+        "numero": "096",
+    },
+    {
+        "name": "hypnomade",
+        "numero": "097",
+    },
+    {
+        "name": "krabby",
+        "numero": "098",
+    },
+    {
+        "name": "krabboss",
+        "numero": "099",
+    },
+    {
+        "name": "voltorbe",
+        "numero": "100",
+    },
+    {
+        "name": "electrode",
+        "numero": "101",
+    },
+    {
+        "name": "noeunoeuf",
+        "numero": "102",
+    },
+    {
+        "name": "noadkoko",
+        "numero": "103",
+    },
+    {
+        "name": "osselait",
+        "numero": "104",
+    },
+    {
+        "name": "ossatueur",
+        "numero": "105",
+    },
+    {
+        "name": "kicklee",
+        "numero": "106",
+    },
+    {
+        "name": "tygnon",
+        "numero": "107",
+    },
+    {
+        "name": "excelangue",
+        "numero": "108",
+    },
+    {
+        "name": "smogo",
+        "numero": "109",
+    },
+    {
+        "name": "smogogo",
+        "numero": "110",
+    },
+    {
+        "name": "rhinocorne",
+        "numero": "111",
+    },
+    {
+        "name": "rhinoféros",
+        "numero": "112",
+    },
+    {
+        "name": "leveinard",
+        "numero": "113",
+    },
+    {
+        "name": "saquedeneu",
+        "numero": "114",
+    },
+    {
+        "name": "kangourex",
+        "numero": "115",
+    },
+    {
+        "name": "hypocean",
+        "numero": "116",
+    },
+    {
+        "name": "poissoroy",
+        "numero": "117",
+    },
+    {
+        "name": "stari",
+        "numero": "118",
+    },
+    {
+        "name": "staross",
+        "numero": "119",
+    },
+    {
+        "name": "m. mime",
+        "numero": "122",
+    },
+    {
+        "name": "insecateur",
+        "numero": "123",
+    },
+    {
+        "name": "lippoutou",
+        "numero": "124",
+    },
+    {
+        "name": "elektek",
+        "numero": "125",
+    },
+    {
+        "name": "magmar",
+        "numero": "126",
+    },
+    {
+        "name": "scarabrute",
+        "numero": "127",
+    },
+    {
+        "name": "tauros",
+        "numero": "128",
+    },
+    {
+        "name": "magicarpe",
+        "numero": "129",
+    },
+    {
+        "name": "léviator",
+        "numero": "130",
+    },
+    {
+        "name": "lokhlass",
+        "numero": "131",
+    },
+    {
+        "name": "métamorph",
+        "numero": "132",
+    },
+    {
+        "name": "evoli",
+        "numero": "133",
+    },
+    {
+        "name": "aquali",
+        "numero": "134",
+    },
+    {
+        "name": "voltali",
+        "numero": "135",
+    },
+    {
+        "name": "pyroli",
+        "numero": "136",
+    },
+    {
+        "name": "porygon",
+        "numero": "137",
+    },
+    {
+        "name": "amonita",
+        "numero": "138",
+    },
+    {
+        "name": "amonistar",
+        "numero": "139",
+    },
+    {
+        "name": "kabuto",
+        "numero": "140",
+    },
+    {
+        "name": "kabutops",
+        "numero": "141",
+    },
+    {
+        "name": "ptera",
+        "numero": "142",
+    },
+    {
+        "name": "ronflex",
+        "numero": "143",
+    },
+    {
+        "name": "artikodin",
+        "numero": "144",
+    },
+    {
+        "name": "electhor",
+        "numero": "145",
+    },
+    {
+        "name": "sulfura",
+        "numero": "146",
+    },
+    {
+        "name": "minidraco",
+        "numero": "147",
+    },
+    {
+        "name": "draco",
+        "numero": "148",
+    },
+    {
+        "name": "dracolosse",
+        "numero": "149",
+    },
+    {
+        "name": "mewtwo",
+        "numero": "150",
+    },
+    {
+        "name": "mew",
+        "numero": "151",
+    },
+];
